65f340aeeaf940ff954f058d6c52776821c9a713
saharVac <vcsahar@gmail.com>
2020-10-05 09:25:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// Questions from https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+
+var questions = [
+  {
+    q: "Which of the following correctly describe JavaScript?",
+    a: [
+      "It is a lightweight, interpreted programming language",
+      "It has object oriented capabilities that allows you to build interactivity into otherwise static HTML pages",
+      "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers",
+      "All of the above",
+    ],
+  },
+  {
+    q:
+      "Which of the following is a valid type of function javascript supports?",
+    a: [
+      "named function",
+      "anonymous function",
+      "Both of the above",
+      "None of the above",
+    ],
+  },
+  {
+    q: "Which built-in method returns the length of the string?",
+    a: ["length()", "size()", "index()", "None of the above"],
+  },
+  {
+    q:
+      "Which built-in method returns the calling string value converted to upper case? ",
+    a: ["toUpperCase()", "toUpper()", "changeCase(case)", "None of the above."],
+  },
+  {
+    q:
+      "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+    a: ["valueOf()", "toString()", "None of the above.", "toSource()"],
+  },
+  {
+    q:
+      "Which of the following function of String object returns a number indicating whether a reference string comes before or after or is the same as the given string in sort order?",
+    a: ["search()", "localeCompare()", "substr()", "concat()"],
+  },
+  {
+    q:
+      "Which of the following function of String object returns the primitive value of the specified object?",
+    a: ["toLocaleUpperCase()", "toUpperCase()", "toString()", "valueOf()"],
+  },
+  {
+    q:
+      "Which of the following function of String object causes a string to be displayed in a small font, as if it were in a <small> tag?",
+    a: ["link()", "small()", "sup()", "sub()"],
+  },
+  {
+    q:
+      "Which of the following function of Array object calls a function for each element in the array?",
+    a: ["concat()", "every()", "forEach()", "filter()"],
+  },
+  {
+    q:
+      "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
+    a: ["unshift()", "sort()", "splice()", "toString()"],
+  },
+];
+
+var correct = [3, 2, 0, 0, 3, 1, 3, 1, 2];
