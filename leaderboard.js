dd3f8d5abe2f15822a2cfa4f7fddd0fe15b62f3f
saharVac <vcsahar@gmail.com>
2020-10-07 18:18:58 -0400

    Added functionality to diplay scores from local storage

---
+++ leaderboard.js
@@ -0,0 +1,25 @@
+// DOM Elements
+var initialsList = document.getElementById("initials-list");
+var scoresList = document.getElementById("scores-list");
+var datesList = document.getElementById("dates-list");
+
+// iterating over scores in local storage
+for (let i = 0; i < localStorage.length; i++) {
+  // get object format of stored info
+  var item = JSON.parse(localStorage[i]);
+
+  // create li elements for initials, score, and date
+  var initials = document.createElement("li");
+  var score = document.createElement("li");
+  var date = document.createElement("li");
+  console.log(scoresList);
+  // give list elements info from item
+  initials.textContent = item.init;
+  score.textContent = item.points;
+  date.textContent = item.date;
+
+  // place list items on the page
+  initialsList.appendChild(initials);
+  scoresList.appendChild(score);
+  datesList.appendChild(date);
+}
