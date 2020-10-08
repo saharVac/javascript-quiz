c72244a4611528601b63d388e3383194597caf44
saharVac <vcsahar@gmail.com>
2020-10-07 20:39:47 -0400

    Adjusted click to work on choice container rather than just text

--- script.js
+++ script.js
@@ -80,9 +80,11 @@ var correct = [0, 2, 2, 0, 0, 1, 2, 2, 3, 3];
 
 // Functionality for choices
 for (let i = 1; i <= 4; i++) {
-  document.getElementById("choice-" + i).addEventListener("click", function () {
-    chosen = i;
-  });
+  document
+    .getElementById("choice-" + i)
+    .parentElement.addEventListener("click", function () {
+      chosen = i;
+    });
 }
 
 // Submitting score
